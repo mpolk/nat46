26401644d61cb5a00202fc995beadafa187e40a8
mpolk <dmitriy.stepanenko@gmail.com>
2024-06-04 15:30:26 +0300

    Implementing coredns plugin

--- coredns-plugin/nat46.go
+++ coredns-plugin/nat46.go
@@ -47,7 +47,7 @@ func NewNat46(domainsFileName string, ipv6Prefix string, nat46Device string, ups
 	for scanner.Scan() {
 		domain := strings.Split(scanner.Text(), ".")
 		slices.Reverse(domain)
-		log.Infof("nat46 domain: %v", domain)
+		log.Debugf("nat46 domain: %v", domain)
 		domains = append(domains, domain)
 	}
 
@@ -55,22 +55,22 @@ func NewNat46(domainsFileName string, ipv6Prefix string, nat46Device string, ups
 	if err != nil {
 		return Nat46{}, plugin.Error(PluginName, err)
 	}
-	log.Infof("IPV6 prefix: %v", prefix)
-	log.Infof("NAT46 deivice: '%s'", nat46Device)
+	log.Debugf("IPV6 prefix: %v", prefix)
+	log.Debugf("NAT46 deivice: '%s'", nat46Device)
 
 	return Nat46{domains: domains, ipv6Prefix: *prefix, nat46Device: nat46Device, upstream: upstream}, nil
 }
 
 // ServeDNS implements the plugin.Handler interface. This method gets called when nat46 is used
 // in a Server.
-func (nat46 Nat46) ServeDNS(ctx context.Context, responseWriter dns.ResponseWriter, request *dns.Msg) (int, error) {
-	// This function could be simpler. I.e. just fmt.Println("nat46") here, but we want to show
-	// a slightly more complex example as to make this more interesting.
-	// Here we wrap the dns.ResponseWriter in a new ResponseWriter and call the next plugin, when the
-	// answer comes back, it will print "nat46".
-
+func (nat46 Nat46) ServeDNS(ctx context.Context, responseWriter dns.ResponseWriter, dnsMsg *dns.Msg) (int, error) {
 	// Debug log that we've have seen the query. This will only be shown when the debug plugin is loaded.
-	log.Info(fmt.Sprintf("Received request: %v", request.Question))
+	log.Debugf(fmt.Sprintf("Received request: %v", dnsMsg.Question))
+
+	// Don't proxy if we don't need to.
+	if !nat46.requestShouldIntercept(&request.Request{W: responseWriter, Req: dnsMsg}) {
+		return nat46.Next.ServeDNS(ctx, responseWriter, dnsMsg)
+	}
 
 	// Wrap.
 	pw := NewResponsePrinter(responseWriter)
@@ -79,11 +79,53 @@ func (nat46 Nat46) ServeDNS(ctx context.Context, responseWriter dns.ResponseWrit
 	requestCount.WithLabelValues(metrics.WithServer(ctx)).Inc()
 
 	// Call next plugin (if any).
-	return plugin.NextOrFailure(nat46.Name(), nat46.Next, ctx, pw, request)
+	return plugin.NextOrFailure(nat46.Name(), nat46.Next, ctx, pw, dnsMsg)
 }
 
 // Name implements the Handler interface.
 func (nat46 Nat46) Name() string { return PluginName }
+
+// requestShouldIntercept returns true if the request represents one that is eligible
+// for NAT46 processing:
+// 1. The request came in over IPv4
+// 2. The request is of type A
+// 3. The request is of class INET
+// 4. The requested name matches one of the NAT46 domain prefixes
+func (nat46 *Nat46) requestShouldIntercept(req *request.Request) bool {
+	// Make sure that request came in over IPv4
+	if req.Family() != 1 || req.QType() != dns.TypeA || req.QClass() != dns.ClassINET {
+		log.Debugf("Ignore queries of this family (%d), type (%d) or class(%d)", req.Family(), req.QType(), req.QClass())
+		return false
+	}
+
+	chunks := strings.Split(req.QName(), ".")
+	slices.Reverse(chunks)
+	log.Debugf("QName: %v", chunks)
+	for _, nattedDomain := range nat46.domains {
+		log.Debugf("Trying to match '%v'", nattedDomain)
+		i := 0
+		for _, chunk := range chunks {
+			if chunk == "" {
+				continue
+			}
+			if i >= len(nattedDomain) {
+				break
+			}
+			log.Debugf("i: %d, chunk: '%s', nattedDomain[i]: %s", i, chunk, nattedDomain[i])
+			if chunk != nattedDomain[i] {
+				break
+			}
+			i++
+		} //for
+		if i >= len(nattedDomain) {
+			log.Debug("Query matches, should NAT it")
+			return true
+		}
+	} //for
+
+	log.Debug("Query does not match any NATted domain, ignore it")
+	return false
+}
 
 // ResponsePrinter wrap a dns.ResponseWriter and will write nat46 to standard output when WriteMsg is called.
 type ResponsePrinter struct {
